a73094b44df3a564f77dd6d5c841dd9ef32616e6
mkchiraggupta <mkchirag.edu11@gmail.com>
2025-12-07 16:35:09 +0530

    remove - demo  - files

--- src/app/page.js
+++ src/app/page.js
@@ -1,15 +1,9 @@
-import FilterBar from "./Components/FilterBar";
-import FlowExample from "./Components/FlowExample";
-import MultiDropdown from "./Components/MultiDropdown";
 import QuizApp from "./Components/QuizApp";
 
 export default function Home() {
   return (
     <>
-    <FilterBar />
-    {/* <MultiDropdown /> */}
-    {/* <FlowExample /> */}
-      {/* <QuizApp /> */}
+      <QuizApp />
     </>
   );
 }
